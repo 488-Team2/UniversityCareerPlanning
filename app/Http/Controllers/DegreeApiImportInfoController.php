81df93775d61854954edde98c2404db62f4de2c7
dougdoner <ddoner02@gmail.com>
2021-04-21 19:54:27 -0700

    Moves API import info to controller

--- app/Http/Controllers/DegreeApiImportInfoController.php
+++ app/Http/Controllers/DegreeApiImportInfoController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Degree;
 use App\Models\DegreeApiImportInfo;
+use App\Models\Department;
+use Faker\Factory as Faker;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 
 class DegreeApiImportInfoController extends Controller
 {
@@ -72,5 +77,59 @@ class DegreeApiImportInfoController extends Controller
     public function destroy(DegreeApiImportInfo $degreeApiImportInfo)
     {
         //
+    }
+
+    public function runImport() {
+        $this->parseDegreeInformation();
+    }
+
+    public function findJsonValue($item, $itemKey)
+    {
+        $itemKeyArray = explode(".", $itemKey);
+        if (count($itemKeyArray) > 1) {
+            if (Arr::isAssoc($item)) {
+                return $this->findJsonValue($item[$itemKeyArray[0]], implode(".", array_slice($itemKeyArray, 1)));
+            } else {
+                return $this->findJsonValue($item[0], implode(".", array_slice($itemKeyArray, 1)));
+            }
+        } else {
+            if (Arr::isAssoc($item)) {
+                return $item[$itemKey];
+            } else {
+                return $item[0][$itemKey];
+            }
+        }
+    }
+
+    public function parseDegreeInformation()
+    {
+        $degreeApiImportInfoEntries = DegreeApiImportInfo::all();
+        $response = Http::get('https://www.keene.edu/catalog/api/programs/');
+        $decoded = json_decode($response->body(), true);
+        $degreeCollection = collect($decoded['data']);
+        $degreeCollection->each(function ($item) use ($degreeApiImportInfoEntries) {
+            $faker = Faker::create();
+
+            //creates an associative array
+            $itemArray = [];
+
+            //For each item in $degreeApiImportInfoEntries, use findJsonValue function to assign to associative array
+            $degreeApiImportInfoEntries->each(function ($entry) use ($item, &$itemArray) {
+                //echo $entry->data_type;
+                if ($entry->data_type == 'department_id') {
+                    $itemArray['department_id'] = Department::firstOrCreate([
+                        'department_name' => $this->findJsonValue($item, $entry->data_label)
+                    ])->id;
+                }
+                else {
+                    $itemArray[$entry->data_type] = $this->findJsonValue($item, $entry->data_label);
+                }
+            });
+
+            $itemArray['degree_code'] = ['R', 'I', 'A', 'S', 'E', 'C'][array_rand(['R', 'I', 'A', 'S', 'E', 'C'])];
+            $itemArray['graduation_rate'] = $faker->numberBetween(0, 100);
+            $itemArray['job_demand'] = $faker->numberBetween(0, 100);
+            Degree::create($itemArray);
+        });
     }
 }
