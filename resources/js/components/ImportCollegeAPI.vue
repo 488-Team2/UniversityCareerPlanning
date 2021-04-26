6138172eb5f542158a8fb545d6d09b7b1a561ae3
dougdoner <ddoner02@gmail.com>
2021-04-26 15:38:55 -0700

    Improving structure of Vue component

--- resources/js/components/ImportCollegeAPI.vue
+++ resources/js/components/ImportCollegeAPI.vue
@@ -9,11 +9,11 @@
                 tags with ones from your college.</p>
             <label for="collegeApiUrl">College API URL: </label> <input type="text" id="collegeApiUrl"
                                                                         placeholder="API URL"
-                                                                        v-model="apiURL">
+                                                                        v-model="apiURL" required>
             <div class="attributeBox rounded text-center fs-4 shadow-sm" v-for="tag in tableAttributes">
-                <input type="checkbox" v-on:click="selectAttribute" :name="tag" :checked="populatedValue(tag).length > 0">
-                Name: {{ tag }}
-                <input type="text" :placeholder="tag" v-bind:value="populatedValue(tag)">
+                <input type="checkbox" v-model="tag.isChecked" :name="tag.data_type">
+                Name: {{ tag.data_type }}
+                <input type="text" :placeholder="tag.data_type" v-model="tag.data_label">
             </div>
             <button v-on:click.prevent="submitSelectedAttributes" class="btn btn-primary btn-center">Submit changes
             </button>
@@ -29,62 +29,76 @@ export default {
     async created() {
         await this.fetchStoredAttributes();
         await this.fetchAttributes();
+        this.setChecked();
     },
     methods: {
         async fetchAttributes() {
             return axios.get('/api/degreetags').then(response => {
-                this.tableAttributes = response.data;
+                this.tableAttributes = response.data.map(item => {
+                    let obj = {};
+                    obj.data_type = item;
+                    obj.isChecked = false;
+                    return obj;
+                });
             });
         },
         fetchStoredAttributes() {
             return axios.get('/api/degreeimportdata').then(data => {
                 this.tableValues = data.data.data;
+                console.log(this.tableValues[0].data_label);
+                for (let i = 0; i < this.tableAttributes.length; i++) {
+                    this.tableValues.forEach(function (item) {
+                        if (this.tableAttributes[i].data_type === item.data_type) {
+                            this.tableAttributes[i].data_label = item.data_label;
+                            console.log("we did it");
+                        }
+                    });
+                }
                 this.apiURL = this.populatedValue("apiURL");
             });
         },
         populatedValue(attribute) {
             let found = false;
-            let value = "";
+            console.log(attribute.data_type);
             for (let i = 0; i < this.tableValues.length && !found; i++) {
-                if (this.tableValues[i].data_type === attribute) {
+                if (this.tableValues[i].data_type === attribute.data_type && this.tableValues[i].data_label !== "") {
+                    //console.log(this.tableValues[i].data_label);
                     found = true;
-                    value = this.tableValues[i].data_label;
+                    attribute.data_label = this.tableValues[i].data_label;
                 }
             }
-            return value;
-        },
-        isFieldPopulated(event) {
-            if (event.target.checked)
-                console.log("hello");
-            if (event.target.nextElementSibling.value.length > 0) {
-                console.log(event.target.nextElementSibling.value.length);
-                return true;
-            } else {
-                return false;
-            }
+            return attribute.data_label;
         },
         setPopulatedValue(attribute, value) {
             let found = false;
             for (let i = 0; i < this.tableValues.length && !found; i++) {
                 if (this.tableValues[i].data_type === attribute) {
+                    console.log("Found!");
                     found = true;
                     this.tableValues[i].data_label = value;
                 }
             }
         },
-        selectAttribute(event) {
-            if (event.target.checked) {
+        selectAttributes() {
+            let checkedAttributes = this.tableAttributes.filter(item => item.isChecked);
+            checkedAttributes.forEach(item => {
+                this.selectedAttributes.push(item);
+            });
+            /*if (event.target.checked) {
                 this.selectedAttributes.push(event.target);
             } else {
                 let itemIndex = this.selectedAttributes.indexOf(event.target);
                 this.selectedAttributes.splice(itemIndex, 1);
-            }
+            }*/
         },
         submitSelectedAttributes() {
             let submittedAttributes = [];
+            this.selectAttributes();
             this.selectedAttributes.forEach((item) => {
-                submittedAttributes.push({"itemName": item.name, "itemValue": item.nextElementSibling.value});
-                this.setPopulatedValue(item.name, item.nextElementSibling.value);
+                console.log(item.data_type);
+                console.log(item.data_label);
+                submittedAttributes.push({"itemName": item.data_type, "itemValue": item.data_label});
+                this.setPopulatedValue(item.data_type, item.data_label);
             });
             submittedAttributes.push({"itemName": "apiURL", "itemValue": this.apiURL});
             axios({
@@ -103,7 +117,22 @@ export default {
                 this.alertType = "alert-danger";
                 this.alerts.push(value[0]);
             });
+        },
+        setChecked() {
+            for (let i = 0; i < this.tableValues.length; i++) {
+                this.tableAttributes.forEach(item => {
+                        if (item.data_type === this.tableValues[i].data_type) {
+                            item.isChecked = !item.isChecked;
+                            console.log("haha that tickles");
+                            return item;
+                        }
+                    }
+                );
+            }
         }
+        //TODO: write method for deleting field (Doug Doner 4/26/2021)
+
+        //TODO: Setup button for importing data using selected fields (Doug Doner 4/26/2021)
     },
     data() {
         return {
